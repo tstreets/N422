b6420feedbecbee9f3acb619d23ea2eccfe77043
Ty Streets <tstreets@iu.edu>
2024-09-18 17:00:34 -0400

    first commit

---
+++ digital-read-write/sketch_sep18a/sketch_sep18a.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // Digital HIGH == 1
+  // Digital LOW == 0
+  // digitalWrite(13, 1);
+  // delay(2000);
+  digitalWrite(13, 0);
+  // delay(2000);
+}
